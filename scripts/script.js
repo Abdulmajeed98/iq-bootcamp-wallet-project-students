598c36f04a6308c17d2d30fb02b697917e20ea65
Abdulmajeed Jaafer <abdulmajeedjaafer18@gmail.com>
2021-05-01 01:09:11 +0300

    wallet-view.html added

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,21 @@
 document.addEventListener("DOMContentLoaded", doThis());
-
+let transactionMode = 0;
 function doThis() {
-  console.log("content Loaded");
-  const createWalletBTN = document.getElementById("create-wallet");
-  createWalletBTN.addEventListener("click", () => {
-    console.log("btn clicked");
+  const incomeBTN = document.getElementById("income");
+  const expenseBTN = document.getElementById("expense");
+  incomeBTN.addEventListener("click", (e) => {
+    e.preventDefault();
+    transactionMode = 0;
+    incomeBTN.className = "income-active";
+    expenseBTN.className = "expense-passive";
+    console.log(transactionMode);
+  });
+
+  expenseBTN.addEventListener("click", (e) => {
+    e.preventDefault();
+    transactionMode = 1;
+    incomeBTN.className = "income-passive";
+    expenseBTN.className = "expense-active";
+    console.log(transactionMode);
   });
 }
